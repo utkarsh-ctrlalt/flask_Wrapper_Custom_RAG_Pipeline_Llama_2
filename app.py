1057ed41be09a5ecd6d2e16b6f86a2ee0614fc0d
Utkarsh Singh <singh.utkarsh95@gmail.com>
2024-02-27 00:48:53 +0530

    updated comments and docstrings

--- app.py
+++ app.py
@@ -5,21 +5,42 @@ from pathlib import Path
 import rag_pipeline
 import config
 
+# Resolve the absolute path of the current file and its root directory
 FILE = Path(__file__).resolve()
 ROOT = FILE.parents[0]
 
 
 def create_app(testing : bool = True):
+    """
+    Create and configure the Flask application.
+
+    Args:
+    testing (bool, optional): Flag indicating whether the application is created for testing purposes. Defaults to True.
+
+    Returns:
+    Flask: Configured Flask application.
+    """
+    # Load configuration parameters from config.py
     model_id = config.summerizer_model_id
     hf_auth_token = config.hf_token
     store_dir_path = config.store_dir_path
 
+    # Initialize the model and database objects using RAG pipeline
     llm = rag_pipeline.getModel(model_id, hf_auth_token)
     db = rag_pipeline.database(store_dir_path)
+
+    # Initialize Flask application
     app = Flask(__name__)
 
     @app.route("/", methods=["GET"])
     def version_info():
+        """
+        Provide version information about the service.
+
+        Returns:
+        jsonify: JSON response containing version information.
+        """
+
         return jsonify([{
             'Version': '1.0', 
             'ServiceName': 'Custom RAG Pipeline', 
@@ -28,13 +49,19 @@ def create_app(testing : bool = True):
 
     @app.route('/infer', methods=["POST"])
     def pred_fun():
+        """
+        Endpoint to process input data and generate a response.
+
+        Returns:
+        jsonify: JSON response containing the processed string.
+        """
         # Get the JSON data from the request
         json_data = request.get_json()
 
-        # Assuming the JSON contains a key called 'input_string'
+        # Read the JSON which contains a key called 'input_string'
         input_string = json_data.get('input_string', '')
 
-        # Process the input_string (Here, let's just return it as uppercase)
+        # Process the input_string 
         processed_string = rag_pipeline.generate_response(input_string, llm=llm, db=db)
 
         # Return the processed string as a JSON response
